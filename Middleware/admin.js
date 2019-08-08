915866b1f768c2d574a0be68eb1fbbd3bc336dfa
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-08-08 13:01:01 -0400

    Fixed admin middleware

--- Middleware/admin.js
+++ Middleware/admin.js
@@ -3,7 +3,7 @@ const USER = require('../models/user-model');
 
 module.exports = (req, res, next) => {
     JWT.verify(req.headers.authorization, process.env.JWT_SECRET, (err, decoded) => {
-        if (!err && decoded && decoded.id === '5d4c52a48fc9c30e00ab7730') {
+        if (!err && decoded && decoded.id === process.env.ADMIN) {
             USER.findById(decoded.id)
                 .then(foundUser => {
                     if (!foundUser && err) throw err;
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
         } else {
             req.errors = err
             return res.status(401).json({
-                message: '🛑 ADMIN ONLY ACCESSABLE ROUTE 🛑',
+                message: 'ADMIN ONLY ACCESSIBLE ROUTE',
                 status: 401
             })
         }
